fdf4fa4bcf00f8fd320ac0751af8979064e9b35e
Sebastian <anse23@hotmail.com>
2018-04-13 20:29:39 +0100

    feat: Added game in fragment using a WebView

--- AnkiGame/app/src/main/java/com/keemsa/ankigame2048/features/start/MainFragment.java
+++ AnkiGame/app/src/main/java/com/keemsa/ankigame2048/features/start/MainFragment.java
@@ -1,17 +1,32 @@
 package com.keemsa.ankigame2048.features.start;
 
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.keemsa.ankigame2048.R;
 import com.keemsa.ankigame2048.base.BaseFragment;
 import com.keemsa.ankigame2048.util.RxEventBus;
+
+import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -27,19 +42,17 @@ public class MainFragment extends BaseFragment implements MainMvpView{
     @Inject
     RxEventBus mEventBus;
 
-    private ImageView[] selectionIcons;
+    @BindView(R.id.web_main)
+    WebView web_main;
 
-    @BindView(R.id.txt_name)
-    TextView txt_season;
-
-    @BindView(R.id.btn_add)
-    Button btn_add;
-
-
-    @OnClick(R.id.btn_add)
-    public void addMessage() {
-        mPresenter.addMessage(String.valueOf(txt_season.getText()));
-    }
+    private String LOG_TAG = MainFragment.class.getSimpleName();
+    private long mLastBackPress;
+    private static final long mBackPressThreshold = 3500;
+    private static final String IS_FULLSCREEN_PREF = "is_fullscreen_pref";
+    private static boolean DEF_FULLSCREEN = true;
+    private long mLastTouch;
+    private static final long mTouchThreshold = 2000;
+    private Toast pressBackToast;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -51,6 +64,8 @@ public class MainFragment extends BaseFragment implements MainMvpView{
         activityComponent().inject(this);
 
         mPresenter.attachView(this);
+
+        initWebView(savedInstanceState);
 
         return view;
     }
@@ -70,4 +85,140 @@ public class MainFragment extends BaseFragment implements MainMvpView{
             mPresenter.detachView();
         }
     }
+
+    private void initWebView(Bundle savedInstanceState) {
+        // Don't show an action bar or title
+        //getActivity().requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // If on android 3.0+ activate hardware acceleration
+        if (Build.VERSION.SDK_INT >= 11) {
+            getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+        }
+
+        // Apply previous setting about showing status bar or not
+        applyFullScreen(isFullScreen());
+
+        // Check if screen rotation is locked in settings
+        boolean isOrientationEnabled = false;
+        try {
+            isOrientationEnabled = Settings.System.getInt(getContext().getContentResolver(),
+                    Settings.System.ACCELEROMETER_ROTATION) == 1;
+        } catch (Settings.SettingNotFoundException e) {
+            Log.d(LOG_TAG, "Settings could not be loaded");
+        }
+
+        // If rotation isn't locked and it's a LARGE screen then add orientation changes based on sensor
+        int screenLayout = getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK;
+        if (((screenLayout == Configuration.SCREENLAYOUT_SIZE_LARGE)
+                || (screenLayout == Configuration.SCREENLAYOUT_SIZE_XLARGE))
+                && isOrientationEnabled) {
+            getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+        }
+
+        //setContentView(R.layout.activity_main);
+        /*
+        ChangeLog cl = new ChangeLog(this);
+        if (cl.isFirstRun()) {
+            cl.getLogDialog().show();
+        }
+        */
+        // Load webview with game
+        //web_main = (WebView) findViewById(R.id.mainWebView);/
+        WebSettings settings = web_main.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        settings.setDatabasePath(getActivity().getFilesDir().getParentFile().getPath() + "/databases");
+
+        // If there is a previous instance restore it in the webview
+        if (savedInstanceState != null) {
+            // TODO: If app was minimized and Locale language was changed, we need to reload webview with changed language
+            web_main.restoreState(savedInstanceState);
+        } else {
+            // Load webview with current Locale language
+            web_main.loadUrl("file:///android_asset/2048/index.html?lang=" + Locale.getDefault().getLanguage());
+        }
+
+        Toast.makeText(getActivity().getApplication(), "Go full screen", Toast.LENGTH_SHORT).show();
+        // Set fullscreen toggle on webview LongClick
+        web_main.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Implement a long touch action by comparing
+                // time between action up and action down
+                long currentTime = System.currentTimeMillis();
+                if ((event.getAction() == MotionEvent.ACTION_UP)
+                        && (Math.abs(currentTime - mLastTouch) > mTouchThreshold)) {
+                    boolean toggledFullScreen = !isFullScreen();
+                    saveFullScreen(toggledFullScreen);
+                    applyFullScreen(toggledFullScreen);
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mLastTouch = currentTime;
+                }
+                // return so that the event isn't consumed but used
+                // by the webview as well
+                return false;
+            }
+        });
+
+        pressBackToast = Toast.makeText(getContext(), "Press back again to exit",
+                Toast.LENGTH_SHORT);
+    }
+    /**
+     * Saves the full screen setting in the SharedPreferences
+     * @param isFullScreen
+     */
+
+    private void saveFullScreen(boolean isFullScreen) {
+        // save in preferences
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putBoolean(IS_FULLSCREEN_PREF, isFullScreen);
+        editor.commit();
+    }
+
+    private boolean isFullScreen() {
+        return PreferenceManager.getDefaultSharedPreferences(getContext()).getBoolean(IS_FULLSCREEN_PREF,
+                DEF_FULLSCREEN);
+    }
+
+    /**
+     * Toggles the activitys fullscreen mode by setting the corresponding window flag
+     * @param isFullScreen
+     */
+    private void applyFullScreen(boolean isFullScreen) {
+        if (isFullScreen) {
+            getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * Prevents app from closing on pressing back button accidentally.
+     * mBackPressThreshold specifies the maximum delay (ms) between two consecutive backpress to
+     * quit the app.
+     */
+
+    /*@Override
+    public void onBackPressed() {
+        long currentTime = System.currentTimeMillis();
+        if (Math.abs(currentTime - mLastBackPress) > mBackPressThreshold) {
+            pressBackToast.show();
+            mLastBackPress = currentTime;
+        } else {
+            pressBackToast.cancel();
+            super.onBackPressed();
+        }
+    }*/
+
 }
