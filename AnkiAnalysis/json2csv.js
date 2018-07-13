771f21e22bf146692acb15138097dc30db74a09c
Sebastian <anse23@hotmail.com>
2018-07-13 20:51:21 +0100

    analysis: Added function to convert logs from json to csv

--- AnkiAnalysis/json2csv.js
+++ AnkiAnalysis/json2csv.js
@@ -3,30 +3,61 @@
 let anki = require("./ankigame2048-export.json");
 let fs = require('fs');
 
-function retrieveUsers(path) {
-	let properties = ["nickName", "points", "bestScore", "date", "time"];
-	let users = [];
-	let usersJson = anki;
+function retrieveUsers(path, properties) {
+	let objectsJson = anki;
 	let pathBranches = path.split("/");
 	for (let i = 0; i < pathBranches.length; i++) {
-		usersJson = usersJson[pathBranches[i]];
+		objectsJson = objectsJson[pathBranches[i]];
 	}
 
 	let file_name = "data/" + path.replace(/\//g, "_") + ".csv";
 	fs.writeFile(file_name, '', err => {
 		if(err) throw err;
 	});
+	let out = fs.createWriteStream(file_name, {flags: 'a'});
 
-	Object.keys(usersJson).map(function (key) {
-		let currentUser = usersJson[key];
+	Object.keys(objectsJson).map(function (key) {
+		let currentUser = objectsJson[key];
 		let stringCsv = key + ",";
 		for(const prop of properties){
 			stringCsv += getProperty(currentUser, prop) + ",";
 		}
 		stringCsv = stringCsv.slice(0, -1);
 		stringCsv += "\n";
+		out.write(stringCsv, 'utf-8');
 		fs.appendFile(file_name, stringCsv, err => {
 			if(err) throw err;
+		});
+	});
+}
+
+function retrieveLogs(path, properties) {
+	let objectsJson = anki;
+	let pathBranches = path.split("/");
+	for (let i = 0; i < pathBranches.length; i++) {
+		objectsJson = objectsJson[pathBranches[i]];
+	}
+
+	let file_name = "data/" + path.replace(/\//g, "_") + ".csv";
+	fs.writeFile(file_name, '', err => {
+		if(err) throw err;
+	});
+	let out = fs.createWriteStream(file_name, {flags: 'a'});
+
+	Object.keys(objectsJson).map(key => {
+		let currentLogType = objectsJson[key];
+		Object.keys(currentLogType).map(key2 => {
+			let currentLog = currentLogType[key2]
+			let stringCsv = key2 + ",";
+			for(const prop of properties){
+				stringCsv += getProperty(currentLog, prop) + ",";
+			}
+			stringCsv = stringCsv.slice(0, -1);
+			stringCsv += "\n";
+			out.write(stringCsv, 'utf-8');
+			// fs.appendFile(file_name, stringCsv, err => {
+			// 	if(err) throw err;
+			// });
 		});
 	});
 }
@@ -38,8 +69,20 @@ function getProperty(jsonObject, property) {
 	return "";
 }
 
-retrieveUsers("connection/users");
-retrieveUsers("independent/users");
-retrieveUsers("public/connection/users");
-retrieveUsers("public/independent/users");
+let userProperties = ["nickName", "points", "bestScore", "date", "time"];
+retrieveUsers("connection/users", userProperties);
+retrieveUsers("independent/users", userProperties);
+retrieveUsers("public/connection/users", userProperties);
+retrieveUsers("public/independent/users", userProperties);
 
+// let logProperties = ["cardAnswer", "cardEase", "cardInfo", "coinsInCard",
+// "deckInfo", "dueDeckInfo", "earnedCoins", "earnedPoints", "elapsedTime",
+// "favCard", "isFavCard", "logType", "pointsInCard", "totalCoins", "totalPoints",
+// "userId", "date", "time"];
+let logProperties = ["cardEase", "coinsInCard","earnedCoins", "earnedPoints", "elapsedTime",
+"favCard", "isFavCard", "logType", "pointsInCard", "totalCoins", "totalPoints",
+"userId", "date", "time"];
+retrieveLogs("connection/logs", logProperties);
+retrieveLogs("independent/logs", logProperties);
+retrieveLogs("public/connection/logs", logProperties);
+retrieveLogs("public/independent/logs", logProperties);
